c4731891204600d8973875904a6d4f1b1329c8a1
Vuthipong Chureeganon <thecvch@hotmail.com>
2012-09-15 23:19:21 -0700

    alienware

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a0a4eecec6e743bcdfe30f123bd6314105b8161b3ae223e715451ea6ea5fa0fbfb9620aa15ed06d77a171a2e28fd7b174e1159fd396290db8af6dfab7aa883d4'
